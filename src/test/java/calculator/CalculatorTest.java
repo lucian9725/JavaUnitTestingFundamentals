1f2267c416aef52d7f9f1b8388c82a416848c5d4
lucian9725 <olteanu.ionut.lucian@gmail.com>
2025-04-08 18:27:32 +0300

    Second Commit

--- src/test/java/calculator/CalculatorTest.java
+++ src/test/java/calculator/CalculatorTest.java
@@ -1,4 +1,27 @@
 package calculator;
 
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class CalculatorTest {
+
+    private final Calculator calculator = new Calculator();
+
+    @Test
+    public void testAdunare(){
+        System.out.println("testAdunare");
+        double rezultatActual = calculator.adunare(4, 5);
+        // definiti rezultatul actual asteptat -> double rezultatAsteptat = 9;
+        assertEquals(9, rezultatActual);  //assert(expected, actual)
+
+    }
+
+    @Test
+    public void testScadere(){
+        double rezultatActual = calculator.scadere(4, 5);
+        assertEquals(-1, rezultatActual); //assert(expected, actual)
+
+    }
+
 }
